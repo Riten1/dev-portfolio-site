e5b8226333b3c93b7746ca6342c864134d3da9df
riten <griten186@gmail.com>
2025-06-19 14:45:47 +0545

    feat: added homepage configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   webpack: (config: any) => {
     config.resolve.alias.canvas = false;
-
     return config;
   },
   output: "export",
+  basePath: isProd ? "/dev-portfolio-site" : "",
+  assetPrefix: isProd ? "/dev-portfolio-site" : "",
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
   },
 };
 
